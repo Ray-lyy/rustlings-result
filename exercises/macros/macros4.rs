8bf76d7c89cca28328c4b6ab51282bb7a43aa61c
雷雨阳 <2049830626@qq.com>
2023-10-28 14:15:11 +0800

    first commit

---
+++ exercises/macros/macros4.rs
@@ -0,0 +1,16 @@
+// macros4.rs
+// Execute `rustlings hint macros4` or use the `hint` watch subcommand for a hint.
+
+macro_rules! my_macro {
+    () => {
+        println!("Check out my macro!");
+    };
+    ( $x:expr ) => {
+        println!("Look at this other macro: {}", $x);
+    };
+}
+
+fn main() {
+    my_macro!();
+    my_macro!(7777);
+}
